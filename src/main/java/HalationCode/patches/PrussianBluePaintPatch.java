3c68e0c0e52e1d9158526b0c1e7433ddf8008728
Reina <ReinaSHSL@users.noreply.github.com>
2018-11-26 13:18:33 -0800

    Prussian bug fixes

--- src/main/java/HalationCode/patches/PrussianBluePaintPatch.java
+++ src/main/java/HalationCode/patches/PrussianBluePaintPatch.java
@@ -1,12 +1,14 @@
 package HalationCode.patches;
 
 import HalationCode.relics.katawashoujo.PrussianBluePaint;
+import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.SoulboundField;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.curses.AscendersBane;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.RestRoom;
 
 @SpirePatch(
         clz = CardGroup.class,
@@ -20,7 +22,7 @@ public class PrussianBluePaintPatch {
     public static CardGroup Insert (CardGroup __instance, CardGroup retVal) {
         if (AbstractDungeon.player.hasRelic(PrussianBluePaint.ID)) {
             for (AbstractCard c : AbstractDungeon.player.masterDeck.group) {
-                if (!c.cardID.equals(AscendersBane.ID) && c.color == AbstractCard.CardColor.CURSE || c.type == AbstractCard.CardType.CURSE) {
+                if (!c.cardID.equals(AscendersBane.ID) && !SoulboundField.soulbound.get(c) && AbstractDungeon.getCurrRoom() instanceof RestRoom && c.color == AbstractCard.CardColor.CURSE || c.type == AbstractCard.CardType.CURSE) {
                     retVal.addToBottom(c);
                 }
             }
