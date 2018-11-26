b47ce74b4550c986295d77ca5f911157cc9d7e60
Reina <ReinaSHSL@users.noreply.github.com>
2018-11-25 23:53:00 -0800

    fix peace piece

--- src/main/java/HalationCode/powers/NoDamagePower.java
+++ src/main/java/HalationCode/powers/NoDamagePower.java
@@ -1,5 +1,6 @@
 package HalationCode.powers;
 
+import HalationCode.tools.TextureLoader;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -8,41 +9,36 @@ import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
 public class NoDamagePower extends AbstractPower {
-    public static final String POWER_ID = "halation:NoDamagePower";
-    private static final PowerStrings powerStrings;
-    public static final String NAME;
-    public static final String[] DESCRIPTIONS;
+    public static final String POWER_ID = "halation:NoSkillsPower";
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    public NoDamagePower(final AbstractCreature owner) {
-        this.ID = POWER_ID;
+
+    public NoDamagePower(AbstractCreature owner) {
         this.name = NAME;
+        this.ID = POWER_ID;
         this.owner = owner;
-        this.updateDescription();
+        updateDescription();
+        this.img = TextureLoader.getTexture("HalationImages/powers/NoDamagePower.png");
+        this.canGoNegative = false;
+    }
+
+    @Override
+    public void stackPower(int stackAmount)
+    {
+        this.fontScale = 8.0F;
+        this.amount += stackAmount;
+    }
+
+    @Override
+    public void updateDescription()
+    {
+        this.description = (DESCRIPTIONS[0]);
     }
 
     @Override
     public int onLoseHp (int damageAmount) {
         return 0;
-    }
-
-    public void atStartOfTurn() {
-        AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(this.owner, this.owner, this.ID));
-    }
-
-    @Override
-    public void stackPower(final int stackAmount) {
-        this.fontScale = 8.0f;
-        this.amount += stackAmount;
-    }
-
-    @Override
-    public void updateDescription() {
-       this.description = DESCRIPTIONS[0];
-    }
-
-    static {
-        powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
-        NAME = powerStrings.NAME;
-        DESCRIPTIONS = powerStrings.DESCRIPTIONS;
     }
 }
