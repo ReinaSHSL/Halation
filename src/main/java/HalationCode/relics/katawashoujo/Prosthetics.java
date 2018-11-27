e2af7c8593fa96af007b9d534fb82429ec4a5db8
Reina <ReinaSHSL@users.noreply.github.com>
2018-11-26 23:20:21 -0800

    minor stuff

--- src/main/java/HalationCode/relics/katawashoujo/Prosthetics.java
+++ src/main/java/HalationCode/relics/katawashoujo/Prosthetics.java
@@ -35,7 +35,7 @@ public class Prosthetics extends CustomRelic implements OnReceivePowerRelic {
 
     @Override
     public void onEquip() {
-        AbstractDungeon.player.decreaseMaxHealth(25);
+        AbstractDungeon.player.decreaseMaxHealth(50);
     }
 
     @Override
