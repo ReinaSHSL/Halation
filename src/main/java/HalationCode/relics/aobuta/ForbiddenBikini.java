75895cbe00520974b980aafbcd2bb5034d56e23a
Reina <ReinaSHSL@users.noreply.github.com>
2018-12-21 05:12:16 -0800

    bikini polish

--- src/main/java/HalationCode/relics/aobuta/ForbiddenBikini.java
+++ src/main/java/HalationCode/relics/aobuta/ForbiddenBikini.java
@@ -8,6 +8,8 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.EnergyManager;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.ShopRoom;
 
 public class ForbiddenBikini extends CustomRelic {
     public static final String ID = "halation:ForbiddenBikini";
@@ -39,6 +41,13 @@ public class ForbiddenBikini extends CustomRelic {
     public void onUnequip() {
         final EnergyManager energy = AbstractDungeon.player.energy;
         --energy.energyMaster;
+    }
+
+    @Override
+    public void onEnterRoom(AbstractRoom r) {
+        if (r instanceof ShopRoom) {
+            this.flash();
+        }
     }
 
 }
