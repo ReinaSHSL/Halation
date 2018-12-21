163d7a677568f2c6ea87ae3dfae4b24f2086e7e5
Reina <ReinaSHSL@users.noreply.github.com>
2018-12-21 04:31:40 -0800

    finish encyclopedia

--- src/main/java/HalationCode/patches/IncompleteEncyclopediaPatch.java
+++ src/main/java/HalationCode/patches/IncompleteEncyclopediaPatch.java
@@ -6,14 +6,35 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.ui.panels.TopPanel;
 
-@SpirePatch(clz = AbstractCreature.class, method = "renderHealth")
+
 public class IncompleteEncyclopediaPatch {
-    public static SpireReturn Prefix(AbstractCreature __instance, SpriteBatch sb) {
-        if (AbstractDungeon.player.hasRelic(IncompleteEncyclopedia.ID)) {
-            return SpireReturn.Return(null);
-        } else {
-            return SpireReturn.Continue();
+
+    @SpirePatch(clz = AbstractCreature.class,
+            method = "renderHealth"
+    )
+    public static class StopBarRender {
+        public static SpireReturn Prefix(AbstractCreature __instance, SpriteBatch sb) {
+            if (AbstractDungeon.player.hasRelic(IncompleteEncyclopedia.ID)) {
+                return SpireReturn.Return(null);
+            } else {
+                return SpireReturn.Continue();
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = TopPanel.class,
+            method = "renderHP"
+    )
+    public static class StopTopPanelRender {
+        public static SpireReturn Prefix(TopPanel __instance, SpriteBatch sb) {
+            if (AbstractDungeon.player.hasRelic(IncompleteEncyclopedia.ID)) {
+                return SpireReturn.Return(null);
+            } else {
+                return SpireReturn.Continue();
+            }
         }
     }
 }
