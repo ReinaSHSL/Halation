0d33e8803656b3ff6f99458c21bc0d994e853755
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2018-12-22 14:46:18 -0800

    Cat face cupcake

--- src/main/java/HalationCode/relics/ddlc/CatFaceCupcake.java
+++ src/main/java/HalationCode/relics/ddlc/CatFaceCupcake.java
@@ -3,8 +3,12 @@ package HalationCode.relics.ddlc;
 import HalationCode.tools.TextureLoader;
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
+import com.evacipated.cardcrawl.mod.stslib.StSLib;
 import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.EnergyManager;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
@@ -28,5 +32,43 @@ public class CatFaceCupcake extends CustomRelic {
         return new CatFaceCupcake();
     }
 
+    @Override
+    public void onEquip() {
+        final EnergyManager energy = AbstractDungeon.player.energy;
+        ++energy.energyMaster;
+    }
 
+    @Override
+    public void onUnequip() {
+        final EnergyManager energy = AbstractDungeon.player.energy;
+        --energy.energyMaster;
+    }
+
+    @Override
+    public void onUseCard(AbstractCard c, UseCardAction a) {
+        if (this.counter < 0) {
+            this.counter = 0;
+        }
+        this.counter++;
+        if (this.counter > 3) {
+            c.purgeOnUse = true;
+            AbstractCard ca = StSLib.getMasterDeckEquivalent(c);
+            AbstractDungeon.player.masterDeck.removeCard(ca);
+        }
+    }
+
+    @Override
+    public void atTurnStart() {
+        this.counter = 0;
+    }
+
+    @Override
+    public void atBattleStart() {
+        this.counter = 0;
+    }
+
+    @Override
+    public void onVictory() {
+        this.counter = 0;
+    }
 }
