21b5bbf6b06e66001679d09d21a1cd9f35abe232
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2018-12-25 07:05:43 -0800

    Render deck size on icon

--- src/main/java/HalationCode/ui/SimulatedSpireButton.java
+++ src/main/java/HalationCode/ui/SimulatedSpireButton.java
@@ -3,9 +3,14 @@ package HalationCode.ui;
 import HalationCode.relics.ddlc.SimulatedSpire;
 import HalationCode.tools.TextureLoader;
 import basemod.TopPanelItem;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.ui.panels.TopPanel;
 
 public class SimulatedSpireButton extends TopPanelItem {
     private static final Texture IMG = TextureLoader.getTexture("HalationImages/ui/SimulatedSpireButton.png");
@@ -22,6 +27,13 @@ public class SimulatedSpireButton extends TopPanelItem {
             AbstractDungeon.gridSelectScreen.open(r.secondDeck, 999, "Second Deck", false, false, false, false);
             AbstractDungeon.overlayMenu.cancelButton.show("Return");
         }
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        SimulatedSpire r = (SimulatedSpire)AbstractDungeon.player.getRelic(SimulatedSpire.ID);
+        super.render(sb);
+        FontHelper.renderFontRightTopAligned(sb, FontHelper.topPanelAmountFont, Integer.toString(r.secondDeck.size()), this.x + 58.0f * Settings.scale, this.y + 25.0f * Settings.scale, Color.WHITE.cpy());
     }
 
 
